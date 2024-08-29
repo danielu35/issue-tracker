1d37e50a3e5df479d3b35df70eec2a2401f2e1cb
Daniel Uribe <danielu35@gmail.com>
2024-08-29 16:42:56 -0700

    Building the issue status badge

--- app/issues/page.tsx
+++ app/issues/page.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import { Button, Table } from "@radix-ui/themes";
 import Link from "next/link";
 import prisma from "@/prisma/client";
+import IssueStatusBadge from "../component/IssueStatusBadge";
 
 const IssuesPage = async () => {
   const issues = await prisma.issues.findMany();
@@ -36,10 +37,10 @@ const IssuesPage = async () => {
             <Table.Row key={issue.id}>
               <Table.Cell>
                 {issue.title}
-                <div className="block mb:hidden">{issue.status}</div>
+                <div className="block md:hidden">{issue.status}</div>
               </Table.Cell>
               <Table.Cell className="hidden md:table-cell">
-                {issue.status}
+                <IssueStatusBadge status={issue.status} />
               </Table.Cell>
               <Table.Cell className="hidden md:table-cell">
                 {issue.createdAt.toDateString()}
