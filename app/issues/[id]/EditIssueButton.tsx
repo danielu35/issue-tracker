b9e67ec7e8191570b065af4a38774134a49a683b
Daniel Uribe <danielu35@gmail.com>
2024-08-30 15:00:50 -0700

    Applying simple responsibility principle

---
+++ app/issues/[id]/EditIssueButton.tsx
@@ -0,0 +1,19 @@
+import { Pencil2Icon } from "@radix-ui/react-icons";
+import { Button } from "@radix-ui/themes";
+import Link from "next/link";
+import React from "react";
+
+interface Prop {
+  issueId: number;
+}
+
+const EditIssueButton = ({ issueId }: Prop) => {
+  return (
+    <Button>
+      <Pencil2Icon />
+      <Link href={`/issues/${issueId}/edit`}>Edit Issue</Link>
+    </Button>
+  );
+};
+
+export default EditIssueButton;
