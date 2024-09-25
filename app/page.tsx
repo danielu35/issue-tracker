3f7a0b5e8bbafea7e8744e9b0a86a2ede72b8d58
Daniel Uribe <danielu35@gmail.com>
2024-09-24 21:39:23 -0700

    implement pagination

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,21 @@
-import Image from 'next/image'
-import Pagination from './component/Pagination'
+import Image from "next/image";
+import Pagination from "./component/Pagination";
 
-export default function Home() {
+interface Props {
+  searchParams: searchParams;
+}
+
+interface searchParams {
+  page: string;
+}
+
+// export default function Home({ searchParams }: { searchParams: { page: string } }) {
+export default function Home({ searchParams }: Props) {
   return (
-    <Pagination itemCount={100} pageSize={10} currentPage={1} />
-  )
+    <Pagination
+      itemCount={100}
+      pageSize={10}
+      currentPage={parseInt(searchParams.page)}
+    />
+  );
 }
