548fe031899fb09193efaed732f6aa8661878ed0
Daniel Uribe <danielu35@gmail.com>
2024-09-28 23:21:59 -0700

    Build the issue summary component

--- app/page.tsx
+++ app/page.tsx
@@ -1,3 +1,5 @@
+import prisma from "@/prisma/client";
+import IssueSummary from "./IssueSummary";
 import LatestIssues from "./LatestIssues";
 
 // interface Props {
@@ -9,6 +11,11 @@ import LatestIssues from "./LatestIssues";
 // }
 
 // export default function Home({ searchParams }: { searchParams: { page: string } }) {
-export default function Home() {
-  return <LatestIssues />;
+export default async function Home() {
+  const open = await prisma.issues.count({ where: { status: "OPEN" } });
+  const inProgress = await prisma.issues.count({
+    where: { status: "IN_PROGRESS" },
+  });
+  const closed = await prisma.issues.count({ where: { status: "CLOSED" } });
+  return <IssueSummary open={open} inProgress={inProgress} closed={closed} />;
 }
