f993767b031a1f6d06f8059142a1d78ba53e7af4
Daniel Uribe <danielu35@gmail.com>
2024-09-29 16:47:06 -0700

    connected turso online DB

--- prisma/client.ts
+++ prisma/client.ts
@@ -1,15 +1,27 @@
+import { createClient } from '@libsql/client'
+import { PrismaLibSQL } from '@prisma/adapter-libsql'
 import { PrismaClient } from '@prisma/client'
 
-const prismaClientSingleton = () => {
-  return new PrismaClient()
-}
+const libsql = createClient({
+  url: `${process.env.TURSO_DATABASE_URL}`,
+  authToken: `${process.env.TURSO_AUTH_TOKEN}`,
+})
 
-declare const globalThis: {
-  prismaGlobal: ReturnType<typeof prismaClientSingleton>;
-} & typeof global;
-
-const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+const adapter = new PrismaLibSQL(libsql)
+const prisma = new PrismaClient({ adapter })
 
 export default prisma
 
-if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
+// const prismaClientSingleton = () => {
+//   return new PrismaClient()
+// }
+
+// declare const globalThis: {
+//   prismaGlobal: ReturnType<typeof prismaClientSingleton>;
+// } & typeof global;
+
+// const prisma = globalThis.prismaGlobal ?? prismaClientSingleton()
+
+
+
+// if (process.env.NODE_ENV !== 'production') globalThis.prismaGlobal = prisma
